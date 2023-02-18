34bb7dbc4b8c2c8894f870970b305088de3b7d93
JarateKing <scmacintosh@upei.ca>
2023-02-17 23:12:26 -0400

    added basic example

--- examples/basic.py
+++ examples/basic.py
@@ -2,4 +2,12 @@ import sys, os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'src'))
 import dicestats
 
-print('Hello World!')
+# setup our diceroll
+diceroll = dicestats.Diceroll()
+
+# roll 3d6
+diceroll.add(dicestats.Diceroller.rolldice(3, 6))
+
+# print out the chances of each roll
+diceroll.print_probabilities()
+diceroll.plot_probabilities()
