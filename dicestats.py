83ae7296ab3131b453e600b174f0ab5087708832
JarateKing <scmacintosh@upei.ca>
2023-02-17 22:19:29 -0400

    added print_at_most

--- dicestats.py
+++ dicestats.py
@@ -158,6 +158,20 @@ class Diceroll:
         
         print()
     
+    def print_at_most(self, precision = 4):
+        self.__sort_probabilities()
+        maxvaluewidth = 0
+        
+        for value in self.probabilities.keys():
+            maxvaluewidth = max(maxvaluewidth, len(str(value)))
+        
+        current = 0
+        for value, probability in self.probabilities.items():
+            current += probability
+            print("{0: >{2}d}: {1:{4}.{3}f}%".format(value, current * 100, maxvaluewidth, precision, precision + 4))
+        
+        print()
+    
     def plot_probabilities(self, width = 100, barchar = '#', relativeBars = False):
         self.__sort_probabilities()
         maxvaluewidth = 0
@@ -405,4 +419,5 @@ diceroll.apply_probability(RawDiceroller.rolldice(2, 10))
 diceroll = diceroll.convert(RawConvert.maxShared)
 diceroll.print_probabilities()
 diceroll.print_at_least()
+diceroll.print_at_most()
 diceroll.plot_probabilities(relativeBars = True)
