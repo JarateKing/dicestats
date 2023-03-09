844995eaac32562f7d6f2c751a03572e16d19a72
JarateKing <scmacintosh@upei.ca>
2023-03-09 08:50:11 -0400

    fixed erroneous order of methods

--- examples/commoncount.py
+++ examples/commoncount.py
@@ -7,7 +7,7 @@ rawroll = dicestats.RawDiceroll()
 rawroll.apply_probability(dicestats.RawDiceroller.rolldice(10, 4))
 
 # convert to single values, by counting how many dice share a number
-diceroll.print_probabilities()
 diceroll = dicestats.Diceroll()
 diceroll.set_probabilities(rawroll.convert_probabilities(dicestats.RawConvert.maxShared))
+diceroll.print_more_than()
 diceroll.plot_probabilities(relativeBars = True)
